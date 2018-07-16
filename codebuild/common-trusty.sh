269b98362cfc5abe43464080a758ccd8f9c81283
Colden Cullen <colden@amazon.com>
2018-07-16 15:17:20 -0700

    [CI] Enable fuzz testing in CodeBuild

--- codebuild/common-trusty.sh
+++ codebuild/common-trusty.sh
@@ -8,15 +8,40 @@ git clone https://github.com/awslabs/aws-c-common.git
 cd aws-c-common
 mkdir build
 cd build
+
 cmake -DCMAKE_INSTALL_PREFIX=../../install ../
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
+
 make install
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
+
 cd ../..
 
 cd aws-c-compression
 mkdir build
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=../../install ../
+cmake -DCMAKE_INSTALL_PREFIX=../../install $@ ../
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
+
 make
-make test
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
+
+LSAN_OPTIONS=verbosity=1:log_threads=1 ctest --output-on-failure
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
+
 cd ..
+
 ./cppcheck.sh ../install/include
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
