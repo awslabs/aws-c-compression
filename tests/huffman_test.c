3a87f136884d9cdb2d974a68151bf91a8ffae93b
Colden Cullen <colden@amazon.com>
2019-06-12 11:33:36 -0700

    clang-format

--- tests/huffman_test.c
+++ tests/huffman_test.c
@@ -347,7 +347,8 @@ static int test_huffman_transitive(struct aws_allocator *allocator, void *ctx) {
     /* Test encoding a short url and immediately decoding it */
 
     const char *error_message = NULL;
-    int result = huffman_test_transitive(test_get_coder(), s_url_string, URL_STRING_LEN, ENCODED_URL_LEN, &error_message);
+    int result =
+        huffman_test_transitive(test_get_coder(), s_url_string, URL_STRING_LEN, ENCODED_URL_LEN, &error_message);
     ASSERT_SUCCESS(result, error_message);
 
     return AWS_OP_SUCCESS;
@@ -374,7 +375,8 @@ static int test_huffman_transitive_all_code_points(struct aws_allocator *allocat
      * characters and immediately decoding it */
 
     const char *error_message = NULL;
-    int result = huffman_test_transitive(test_get_coder(), s_all_codes, ALL_CODES_LEN, ENCODED_CODES_LEN, &error_message);
+    int result =
+        huffman_test_transitive(test_get_coder(), s_all_codes, ALL_CODES_LEN, ENCODED_CODES_LEN, &error_message);
     ASSERT_SUCCESS(result, error_message);
 
     return AWS_OP_SUCCESS;
@@ -391,8 +393,8 @@ static int test_huffman_transitive_chunked(struct aws_allocator *allocator, void
         const size_t step_size = s_step_sizes[i];
 
         const char *error_message = NULL;
-        int result =
-            huffman_test_transitive_chunked(test_get_coder(), s_all_codes, ALL_CODES_LEN, ENCODED_CODES_LEN, step_size, &error_message);
+        int result = huffman_test_transitive_chunked(
+            test_get_coder(), s_all_codes, ALL_CODES_LEN, ENCODED_CODES_LEN, step_size, &error_message);
         ASSERT_SUCCESS(result, error_message);
     }
 
