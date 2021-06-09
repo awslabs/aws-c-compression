b540718e8f95e59f8dffac1a57633268e623d421
Justin Boswell <boswej@amazon.com>
2021-06-09 21:08:23 +0000

    Fixed shared-lib build

--- include/aws/testing/compression/huffman.h
+++ include/aws/testing/compression/huffman.h
@@ -68,6 +68,7 @@ struct huffman_test_code_point {
  * \return AWS_OP_SUCCESS on success, AWS_OP_FAILURE on failure (error_string
  * will be set)
  */
+AWS_COMPRESSION_API
 int huffman_test_transitive(
     struct aws_huffman_symbol_coder *coder,
     const char *input,
@@ -90,6 +91,7 @@ int huffman_test_transitive(
  * \return AWS_OP_SUCCESS on success, AWS_OP_FAILURE on failure (error_string
  * will be set)
  */
+AWS_COMPRESSION_API
 int huffman_test_transitive_chunked(
     struct aws_huffman_symbol_coder *coder,
     const char *input,
